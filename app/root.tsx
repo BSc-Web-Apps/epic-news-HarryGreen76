b35581e3d3ba494be9678c955e9901a0417c718f
HarryGreen76 <152405544+HarryGreen76@users.noreply.github.com>
2025-11-06 12:10:39 +0000

    Begin Team member Card
    1st Coding Lesson YAY

--- app/root.tsx
+++ app/root.tsx
@@ -4,6 +4,7 @@ import { type loader } from './__root.server.tsx'
 import { GeneralErrorBoundary } from './components/error-boundary.tsx'
 import Document from './components/shared-layout/Document.tsx'
 import { useNonce } from './utils/nonce-provider.ts'
+import portrait1 from '~/assets/jpg/meditation.jpg'
 import rootLinkElements from './utils/providers/rootLinkElements.ts'
 
 export const links: Route.LinksFunction = () => {
@@ -21,7 +22,15 @@ export default function App() {
 			<div className="flex h-screen flex-col justify-between">
 				<div className="flex-1">
 					<main className="grid h-full place-items-center">
-						<h1 className="text-mega">Welcome to Epic News!</h1>
+						<h1 className="text-mega text-fuchsia-700">Your Journey Begins!</h1>
+						<p className="text-base text-gray-600 md:text-lg lg:text-2xl">
+							Welcome to Epic News, where the latest developments in tech are
+							found.
+						</p>
+						<button className="rounded-md bg-blue-500 px-4 py-2 text-white md:px-6 md:py-3 lg:px-8 lg:py-4">
+							Learn More
+						</button>
+						<img src={portrait1} alt="portrait1" />
 					</main>
 				</div>
 			</div>
