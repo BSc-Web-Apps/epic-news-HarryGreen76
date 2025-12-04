0292c7dc92cc9b40a9ebbbfe42f226baa48c6e28
HarryGreen76 <152405544+HarryGreen76@users.noreply.github.com>
2025-12-04 12:07:53 +0000

    Add article category selection and display
    Introduces category selection for articles in the editor and displays the category on article pages. Refactors the app layout to move homepage content to the index route, adds CSRF token support, and updates article creation/editing to include categories. Also fetches available categories for use in article forms.

--- app/root.tsx
+++ app/root.tsx
@@ -1,19 +1,16 @@
-import { RiLinkedinBoxFill, RiTwitterXFill } from 'react-icons/ri'
-import { Link, useLoaderData } from 'react-router'
+import { Outlet, useLoaderData } from 'react-router'
+import { AuthenticityTokenProvider } from 'remix-utils/csrf/react'
 import { type Route } from './+types/root.ts'
 import { type loader } from './__root.server.tsx'
 import { GeneralErrorBoundary } from './components/error-boundary.tsx'
 import FooterLogoCentre from './components/organisms/Footer/FooterLogoCentre.tsx'
 import HeaderWithSearch from './components/organisms/HeaderWithSearch'
-import HeroCallToAction from './components/organisms/Hero/HeroCallToAction.tsx'
 import Document from './components/shared-layout/Document.tsx'
+import { useToast } from './components/toaster.tsx'
+import { EpicToaster } from './components/ui/sonner.tsx'
 import { ThemeSwitch, useTheme } from './routes/resources+/theme-switch.tsx'
 import { useNonce } from './utils/nonce-provider.ts'
 import rootLinkElements from './utils/providers/rootLinkElements.ts'
-import headshot2 from '~/assets/jpg/Ash.jpg'
-import headshot1 from '~/assets/jpg/Ben.jpg'
-import headshot from '~/assets/jpg/Daniel.jpg'
-import Hero2 from '~/assets/jpg/hero-2.jpg'
 
 export const links: Route.LinksFunction = () => {
 	return rootLinkElements
@@ -21,95 +18,35 @@ export const links: Route.LinksFunction = () => {
 export { meta } from './__root.client.tsx'
 export { headers, loader } from './__root.server.tsx'
 
-interface TeamMemberCardProps {
-	name: string
-	role: string
-	imageSrc: string
-}
-
-export function TeamMemberCard({ name, role, imageSrc }: TeamMemberCardProps) {
-	return (
-		<div className="w-fit rounded-lg bg-slate-800 p-8">
-			<img
-				src={imageSrc}
-				alt="An employee"
-				className="mx-auto h-64 w-64 rounded-full"
-			/>
-
-			<div className="pt-6">
-				<h3 className="font-semi-bold text-grey text-center">{name}</h3>
-				<p className="pt-1 text-center text-slate-400">{role}</p>
-
-				<div className="flex justify-center gap-4 pt-6 text-slate-400">
-					<RiTwitterXFill />
-					<RiLinkedinBoxFill />
-				</div>
-			</div>
-		</div>
-	)
-}
-
 export default function App() {
 	const data = useLoaderData<typeof loader | null>()
 	const nonce = useNonce()
 	const theme = useTheme()
+	useToast(data?.toast)
 
 	return (
-		<Document theme={theme} nonce={nonce} honeyProps={data?.honeyProps}>
-			<div className="flex h-screen flex-col justify-between">
-				<HeaderWithSearch />
-				<div className="flex-1">
-					<main className="grid h-full place-items-center">
-						<h1 className="text-5xl text-fuchsia-700">Your Journey Begins!</h1>
-
-						<div className="w-full py-16">
-							<HeroCallToAction image={Hero2} imageRight={true}>
-								<div className="flex flex-col gap-8 px-8">
-									<h2 className="text-h2">Welcome to Epic News</h2>
-									<p className="text-lg">
-										Keep up to date with the latest tech news.
-									</p>
-									<Link
-										to="/signin"
-										className="mt-2 inline-block rounded bg-blue-600 px-4 py-2 font-semibold text-white hover:bg-blue-700 focus:ring-2 focus:ring-blue-400 focus:outline-none"
-										aria-label="Sign in to Epic News"
-									>
-										Sign in
-									</Link>
-								</div>
-							</HeroCallToAction>
-						</div>
-
-						<div className="flex gap-4">
-							<TeamMemberCard
-								name="Daniel Reis"
-								role="Senior Designer"
-								imageSrc={headshot}
-							/>
-							<TeamMemberCard
-								name="Ben Green"
-								role="Lead Developer"
-								imageSrc={headshot1}
-							/>
-							<TeamMemberCard
-								name="Ash Coyle"
-								role="Service Desk"
-								imageSrc={headshot2}
-							/>
-						</div>
-
-						<p className="text-base text-gray-600 md:text-lg lg:text-xl">
-							Welcome to Epic News, where the latest developments in tech are
-							found.
-						</p>
-					</main>
+		<AuthenticityTokenProvider token={data?.csrfToken ?? ''}>
+			<Document theme={theme} nonce={nonce} honeyProps={data?.honeyProps}>
+				<div className="flex h-screen flex-col justify-between">
+					<HeaderWithSearch />
+					<div className="flex-1">
+						<Outlet />
+					</div>
+					<div className="container flex justify-between pb-5">
+						<ThemeSwitch userPreference={data?.requestInfo.userPrefs.theme} />
+					</div>
+					<FooterLogoCentre></FooterLogoCentre>
 				</div>
-				<div className="container flex justify-between pb-5">
-					<ThemeSwitch userPreference={data?.requestInfo.userPrefs.theme} />
-				</div>
-				<FooterLogoCentre></FooterLogoCentre>
-			</div>
-		</Document>
+				<EpicToaster
+					closeButton
+					position="bottom-right"
+					theme={theme}
+					expand
+					richColors
+					duration={5000}
+				/>
+			</Document>
+		</AuthenticityTokenProvider>
 	)
 }
 
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,16 +1,85 @@
-import { type MetaFunction } from 'react-router'
+import { RiLinkedinBoxFill, RiTwitterXFill } from 'react-icons/ri'
+import { Link, type MetaFunction } from 'react-router'
+import HeroCallToAction from '#app/components/organisms/Hero/HeroCallToAction.tsx'
+import headshot2 from '~/assets/jpg/Ash.jpg'
+import headshot1 from '~/assets/jpg/Ben.jpg'
+import headshot from '~/assets/jpg/Daniel.jpg'
+import Hero2 from '~/assets/jpg/hero-2.jpg'
 
 export const meta: MetaFunction = () => [{ title: 'Epic News' }]
+
+interface TeamMemberCardProps {
+	name: string
+	role: string
+	imageSrc: string
+}
+
+export function TeamMemberCard({ name, role, imageSrc }: TeamMemberCardProps) {
+	return (
+		<div className="w-fit rounded-lg bg-slate-800 p-8">
+			<img
+				src={imageSrc}
+				alt="An employee"
+				className="mx-auto h-64 w-64 rounded-full"
+			/>
+
+			<div className="pt-6">
+				<h3 className="font-semi-bold text-grey text-center">{name}</h3>
+				<p className="pt-1 text-center text-slate-400">{role}</p>
+
+				<div className="flex justify-center gap-4 pt-6 text-slate-400">
+					<RiTwitterXFill />
+					<RiLinkedinBoxFill />
+				</div>
+			</div>
+		</div>
+	)
+}
 
 export default function Index() {
 	return (
 		<main className="grid h-full place-items-center">
-			<h1 className="text-mega">
-				Hello from{' '}
-				<pre className="prose bg-primary text-primary-foreground rounded-lg p-6">
-					app/routes/_index.tsx
-				</pre>
-			</h1>
+			<h1 className="text-5xl text-fuchsia-700">Your Journey Begins!</h1>
+
+			<div className="w-full py-16">
+				<HeroCallToAction image={Hero2} imageRight={true}>
+					<div className="flex flex-col gap-8 px-8">
+						<h2 className="text-h2">Welcome to Epic News</h2>
+						<p className="text-lg">
+							Keep up to date with the latest tech news.
+						</p>
+						<Link
+							to="/signin"
+							className="mt-2 inline-block rounded bg-blue-600 px-4 py-2 font-semibold text-white hover:bg-blue-700 focus:ring-2 focus:ring-blue-400 focus:outline-none"
+							aria-label="Sign in to Epic News"
+						>
+							Sign in
+						</Link>
+					</div>
+				</HeroCallToAction>
+			</div>
+
+			<div className="flex gap-4">
+				<TeamMemberCard
+					name="Daniel Reis"
+					role="Senior Designer"
+					imageSrc={headshot}
+				/>
+				<TeamMemberCard
+					name="Ben Green"
+					role="Lead Developer"
+					imageSrc={headshot1}
+				/>
+				<TeamMemberCard
+					name="Ash Coyle"
+					role="Service Desk"
+					imageSrc={headshot2}
+				/>
+			</div>
+
+			<p className="text-base text-gray-600 md:text-lg lg:text-xl">
+				Welcome to Epic News, where the latest developments in tech are found.
+			</p>
 		</main>
 	)
 }
